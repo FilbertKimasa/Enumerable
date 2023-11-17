e7c51fe058a251367f6e1f3a9d8effda23498116
FilbertKimasa <filbertdan67@gmail.com>
2023-11-17 18:19:15 +0300

    Implement enumerables

--- mylist_class.rb
+++ mylist_class.rb
@@ -17,12 +17,12 @@ list = MyList.new(1, 2, 3, 4)
 # <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-puts(list.all? { |e| e < 5 }) # true
-puts(list.all? { |e| e > 5 }) # false
+puts "all?: #{list.all? { |e| e < 5 }}" # true
+puts "all?: #{list.all? { |e| e > 5 }}" # false
 
 # Test #any?
-puts(list.any? { |e| e == 2 }) # true
-puts(list.any? { |e| e == 5 }) # false
+puts "any?: #{list.any? { |e| e == 2 }}" # true
+puts "any?: #{list.any? { |e| e == 5 }}" # false
 
 # Test #filter
-puts(list.filter(&:even?))
+puts "filter: #{list.filter(&:even?)}" # [2, 4]
